c8fd8960ac699092dce3802d3ef467dc15592450
nicAmSarah <nicaciosarah085@gmail.com>
2025-02-22 18:47:09 -0300

    conhecendo o python

---
+++ conversaoDeTipos.py
@@ -0,0 +1,5 @@
+numero_conversao = int(input("Digite um numero para convertermos\n"))
+
+print(int(numero_conversao))
+print(float(numero_conversao))
+print(str(numero_conversao))
---
+++ tiposDeDados.py
@@ -0,0 +1,10 @@
+nome = input(str("Qual seu nome:...\n"))
+
+print(nome)
+
+preco = 10
+print(float(preco))
+
+print(preco/2)
+
+print(preco//2)
