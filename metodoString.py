db3f4fa3702ce27957d8ea5e0fe0d74d3da43f92
nicAmSarah <nicaciosarah085@gmail.com>
2025-02-23 09:33:12 -0300

    Metodos da classe String

---
+++ metodoString.py
@@ -0,0 +1,16 @@
+nome = "Sarah"
+
+print(nome.upper())
+print(nome.title())
+print(nome.lower())
+
+curso = "     Python   "
+
+print(curso.strip())
+print(curso.lstrip())
+print(curso.rstrip())
+
+sobrenome = "Amorim"
+
+print(sobrenome.center( 10, "#"))
+print(".".join(curso))
