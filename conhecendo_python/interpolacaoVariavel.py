5d8336e6c94be3dd7f71000ceaeba52c92df96b4
nicAmSarah <nicaciosarah085@gmail.com>
2025-02-23 10:03:12 -0300

    Interpolação de variaveis

---
+++ conhecendo_python/interpolacaoVariavel.py
@@ -0,0 +1,10 @@
+nome = "Sarah"
+sobrenome = "Nicacio"
+idade = 18
+peso = 17.7
+
+print("olá, me chamo %s" %(nome))
+
+print("ola, me chamo {}" .format(nome))
+
+print(f"ola {nome} {sobrenome}, voce tem {idade} e voce pesa {peso:10.2f}")
