27b830a0105c0d108606c6145f61abd91bf11bd4
nicAmSarah <nicaciosarah085@gmail.com>
2025-02-26 09:20:08 -0300

    listas

---
+++ tipos_de_dados/listas.py
@@ -0,0 +1,34 @@
+carro = ['ford', 'porshe', 'toyota']
+
+furta = []
+
+letras = list("python")
+
+numeros = list(range(10))
+
+print(carro)
+
+print(furta)
+
+print(letras)
+
+print(numeros)
+
+print(carro[2])
+
+print(carro[::-1])
+
+matriz =  [
+    [1, 2, 3],
+    [2, 3, 4],
+    [7, 8 ,8]
+]
+
+print(matriz[0][2])
+
+lista = ["p","y","t","o","n"]
+
+print(lista[2:])
+print(lista[:2])
+print(lista[3:2])
+print(lista[::-1])
